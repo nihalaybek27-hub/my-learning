2ee5bddb15ca56f8ac6cc541563093e9063b6c01
nihalaybek27-hub <nihalaybek27@gmail.com>
2026-06-25 11:38:04 +0300

    Add files via upload

---
+++ const final.dart
@@ -0,0 +1,12 @@
+void main(){
+    const string bankNamespecial="VB Bank";
+    const string user1="Bank 1 musteri";
+    const double user1Money=100.00;
+
+    const string user2="Bank 2 musteri";
+     int user2Money=500;
+
+     user2Money=user2Money-user1Money.toint();
+     print("user2Money: $user2Money");
+
+}
