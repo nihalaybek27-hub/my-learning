76288651822cc113e6ce5c4aba656571fec0e7c8
nihalaybek27-hub <nihalaybek27@gmail.com>
2026-06-25 10:50:07 +0300

    Add files via upload

---
+++ lib/dart deneme pratik.dart
@@ -0,0 +1,21 @@
+void main(){
+  print('hello');
+  print('hello+ilsu');
+  print(2+3);
+
+String verininadi="verinin degeri";
+print(verininadi);
+
+//
+String hello="hello5";{
+
+print(hello);
+print(hello);
+print(hello);
+}
+int money=25;
+print(money);
+
+
+
+}
